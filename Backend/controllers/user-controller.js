5345f7e5314642fb5e29103a994e1d89bc73b29a
Ved Tellawar <tellawarved@gmail.com>
2024-06-16 00:14:50 +0530

    setup user router, user model and controller

---
+++ Backend/controllers/user-controller.js
@@ -0,0 +1,12 @@
+import userModel from "../models/userModel.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
+
+// Login user
+const loginUser = async (req, res) => {}
+
+// Register user
+const registerUser = async (req, res) => {}
+
+export { loginUser, registerUser }
